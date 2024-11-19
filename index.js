8f056a891a0a6504cb72df88945faa660dc88578
joyfulben <benfishernow@gmail.com>
2024-11-19 08:33:22 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+const app = express();
+import axios from "axios";
+
+app.get("/", (req, res)=> {
+    res.send("Server is running");
+});
+app.get('/fetch-occupations', async (req, res) => {
+    try {
+        // API URLS
+        const occAPIData = 'https://delaware-app.datausa.io/api/searchLegacy?dimension=PUMS%20Occupation&hierarchy=Detailed%20Occupation&limit=50000';
+        const specOccAPIData = 'http://datausa.io/api/data?drilldowns=Year,State&measures=Average Wage,Average Wage Appx MOE&Record Count>=5&Workforce Status=true&Detailed Occupation='
+        // Make GET request to the third-party API
+        const response = await axios.get(occAPIData);
+        // Make an array of objects containing the occupation id and title.
+        const apiData = response.data.results
+        const occArray = [];
+        for (const occ of apiData) {
+            console.log("Now checking: ",occ.name);
+            if (!occ.id.includes("X")) { // Filter out occupations with "X" in their id
+             
+                occArray.push({ "id": occ.id, "title": occ.name });
+            }
+          }
+        // Send the response data back to the client
+        res.json({total_occupations:occArray.length, occupations: occArray});
+    } catch (error) {
+      // Handle any errors
+      console.error('Error fetching data:', error);
+      res.status(500).json({ message: 'Error fetching data from the API' });
+    }
+  });
+app.listen(4322, console.log("Server started on Port 4322"));
