bdaf29caface6fdabb9760d5f4a12a4ba6c387e8
joyfulben <benfishernow@gmail.com>
2024-12-17 10:14:01 -0700

    Access just id for api call

--- index.js
+++ index.js
@@ -47,8 +47,9 @@ app.get("/initialize-check", async (req, res) => {
     try {
         const sortedList = await initializeApp(); 
         // const sampleCheck = await axios.get(specOccAPIData+sortedList[2].id);
+        const apiID = `${sortedList[2]["id"]}`;
         if (sortedList.length) {
-            res.json({"Status": 200, "Sample Check": (specOccAPIData+sortedList[2]["id"]), "Occupation List": sortedList});
+            res.json({"Status": 200, "Sample Check": apiID, "Occupation List": sortedList});
         } else {
             res.json({"Status": 400, "Message": "No occupations found"});
         }
